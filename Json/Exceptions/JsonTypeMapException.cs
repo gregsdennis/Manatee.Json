4e5693ff1f166bd652a4648554238bc275163ce6
gdennis <gregsdennis@users.noreply.github.com>
2013-04-25 10:39:18 -0500

    Fixed a bug where IJsonCompatible implementations of abstraction types were not calling FromJson() during deserialization.

--- Json/Exceptions/JsonTypeMapException.cs
+++ Json/Exceptions/JsonTypeMapException.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿/***************************************************************************************
+
+	Copyright 2012 Greg Dennis
+
+	   Licensed under the Apache License, Version 2.0 (the "License");
+	   you may not use this file except in compliance with the License.
+	   You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+	   Unless required by applicable law or agreed to in writing, software
+	   distributed under the License is distributed on an "AS IS" BASIS,
+	   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	   See the License for the specific language governing permissions and
+	   limitations under the License.
+ 
+	File Name:		JsonTypeMapException.cs
+	Namespace:		Manatee.Json.Exceptions
+	Class Name:		JsonTypeMapException
+	Purpose:		Thrown when an abstract or interface type is mapped to another
+					abstract or interface type.
+
+***************************************************************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Manatee.Json.Exceptions
 {
+	///<summary>
+	/// Thrown when an abstract or interface type is mapped to another abstract or interface type.
+	///</summary>
+	///<typeparam name="TAbstract">The type being mapped from.</typeparam>
+	///<typeparam name="TConcrete">The type being mapped to.</typeparam>
 	public class JsonTypeMapException<TAbstract, TConcrete> : Exception
 	{
+		/// <summary>
+		/// Creates a new instance of the JsonTypeMapException object.
+		/// </summary>
 		public JsonTypeMapException()
 			: base(string.Format("Cannot create map from type '{0}' to type '{1}' because the destination type is either abstract or an interface.",
 								 typeof(TAbstract),
