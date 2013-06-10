d1100dd06ac286e09e71e249e600026d2d0c6682
gregsdennis <gregsdennis@users.noreply.github.com>
2013-06-10 08:20:34 -0500

    BUG FIX: Type Generation does not support events

--- Json.Tests/Test References/IInterface.cs
+++ Json.Tests/Test References/IInterface.cs
@@ -7,5 +7,6 @@ namespace Manatee.Tests.Test_References
 		string this[int a] { get; set; }
 		string RequiredProp { get; set; }
 		T RequiredMethod<T, U>(U str) where T : U;
+		event EventHandler RequiredEvent;
 	}
 }
