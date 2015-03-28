648717be74b5f0ae1a287e1fe3b73245ac97b8e1
gregsdennis <gregsdennis@users.noreply.github.com>
2015-03-28 18:09:51 -0500

    No functional changes. Removed JetBrains.Annotations from project (could interfere with their Nuget package). Added reference to the Nuget package published by JetBrains.

--- Manatee.Json/Serialization/TypeInstantiationException.cs
+++ Manatee.Json/Serialization/TypeInstantiationException.cs
@@ -25,8 +25,15 @@ using System;
 
 namespace Manatee.Json.Serialization
 {
+	/// <summary>
+	/// Thrown when a type cannot be instantiated.
+	/// </summary>
 	public class TypeInstantiationException : Exception
 	{
+		/// <summary>
+		/// Creates a new instance of the <see cref="TypeInstantiationException"/> class.
+		/// </summary>
+		/// <param name="type">The type which could not be instantiated.</param>
 		public TypeInstantiationException(Type type)
 			: base(string.Format("Manatee.Json cannot create an instance of type '{0}' through the default resolver." +
 								 " You may need to implement your own IResolver to instantiate this type.", type)) {}
