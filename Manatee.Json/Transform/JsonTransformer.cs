233390021f667ebc3624da9dae9b36d747a0b3e3
gregsdennis <gregsdennis@localhost>
2015-09-29 08:17:23 -0500

    BUG FIX: Number formatting should use InvariantCulture.

--- Manatee.Json/Transform/JsonTransformer.cs
+++ Manatee.Json/Transform/JsonTransformer.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿/***************************************************************************************
+
+	Copyright 2015 Greg Dennis
+
+	   Licensed under the Apache License, Version 2.0 (the "License");
+	   you may not use this file except in compliance with the License.
+	   You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+	   Unless required by applicable law or agreed to in writing, software
+	   distributed under the License is distributed on an "AS IS" BASIS,
+	   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	   See the License for the specific language governing permissions and
+	   limitations under the License.
+ 
+	File Name:		JsonTransformer.cs
+	Namespace:		Manatee.Json.Transform
+	Class Name:		JsonTransformer
+	Purpose:		Performs transformations between JSON schemas.
+
+***************************************************************************************/
+
+using System;
 using System.Linq;
 using Manatee.Json.Internal;
 using Manatee.Json.Path;
 
 namespace Manatee.Json.Transform
 {
+	/// <summary>
+	/// Performs transformations between JSON schemas.
+	/// </summary>
 	public static class JsonTransformer
 	{
+		/// <summary>
+		/// Performs transformations between JSON schemas.
+		/// </summary>
+		/// <param name="source">The source JSON.</param>
+		/// <param name="template">A template defining the transformation.</param>
+		/// <returns>The transformed JSON.</returns>
 		public static JsonValue Transform(this JsonValue source, JsonValue template)
 		{
 			return Transform(source, source, template, -1);
