8731c084f592450a33e16e1991c02feb8b74d446
Greg Dennis <greg.dennis@intergen.co.nz>
2016-09-14 15:20:28 +1200

    I've seen the light.

--- Manatee.Json.Tests/Path/FilterExpressionTest.cs
+++ Manatee.Json.Tests/Path/FilterExpressionTest.cs
@@ -12,12 +12,36 @@ namespace Manatee.Json.Tests.Path
 	public class FilterExpressionTest
 	{
 		[TestMethod]
+		[Ignore] // interestingly, it never worked like this...
 		public void KeyEqualsValue()
 		{
 			var text = "$[?(@.test == 5)]";
-			var expected = JsonPathWith.Array(jv => jv.Name("test").Name("second") == 5);
+			var expected = JsonPathWith.Array(jv => jv.Name("test") == 5);
 
 			var actual = JsonPath.Parse(text);
+
+			// this failes because the parsed version uses a raw PathExpression
+			// whereas the constructed version uses a NameExpression (which contains a path)
+			// TODO: Remove PathExpression derivatives.
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod] // this is the same test as above, just that we're validating the
+					 // path evaluation rather than comparing the paths.
+		public void KeyEqualsValue_Evaluation()
+		{
+			var text = "$[?(@.test == 5)]";
+			var json = new JsonArray
+				{
+					new JsonObject {{"test", 5}, {"yep", 6}},
+					new JsonObject {{"test", 6}, {"nope", 6}},
+				};
+			var path = JsonPathWith.Array(jv => jv.Name("test") == 5);
+			var expected = path.Evaluate(json);
+			var parsed = JsonPath.Parse(text);
+
+			var actual = parsed.Evaluate(json);
 
 			Assert.AreEqual(expected, actual);
 		}
