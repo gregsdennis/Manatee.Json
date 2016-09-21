1b862fdb8b856ef8a3efaf017d364a631684f371
Greg Dennis <greg.dennis@intergen.co.nz>
2016-09-21 22:02:15 +1200

    Added apache documentation. Removed remaining references to StateMachine.

--- Manatee.Json/Path/Expressions/Parsing/DivideExpressionParser.cs
+++ Manatee.Json/Path/Expressions/Parsing/DivideExpressionParser.cs
@@ -1,4 +1,26 @@
-﻿namespace Manatee.Json.Path.Expressions.Parsing
+﻿/***************************************************************************************
+
+	Copyright 2016 Greg Dennis
+
+	   Licensed under the Apache License, Version 2.0 (the "License");
+	   you may not use this file except in compliance with the License.
+	   You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+	   Unless required by applicable law or agreed to in writing, software
+	   distributed under the License is distributed on an "AS IS" BASIS,
+	   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	   See the License for the specific language governing permissions and
+	   limitations under the License.
+ 
+	File Name:		DivideExpressionParser.cs
+	Namespace:		Manatee.Json.Path.Expressions.Parsing
+	Class Name:		DivideExpressionParser
+	Purpose:		Parses division expressions within JsonPath.
+
+***************************************************************************************/
+namespace Manatee.Json.Path.Expressions.Parsing
 {
 	internal class DivideExpressionParser : IJsonPathExpressionParser
 	{
