b837a6ea0a1643bcd419cf959d5e42f513b17919
Greg Dennis <gregsdennis@yahoo.com>
2017-04-02 21:18:56 +1200

    further changes to get myget working again

--- build.bat
+++ build.bat
@@ -6,6 +6,8 @@ if "%config%" == "" (
 )
 
 REM Build
+dotnet restore
+
 "%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" Manatee.Json.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 REM packaging takes place as part of post-build of Manatee.Json-4.5 project.
