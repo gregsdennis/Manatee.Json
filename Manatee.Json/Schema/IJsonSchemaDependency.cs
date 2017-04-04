5bbd52158c0e8d920f1d8d9d996162635908d8b0
Greg Dennis <gregsdennis@yahoo.com>
2017-04-04 21:29:08 +1200

    removed remaining apache callouts

--- Manatee.Json/Schema/IJsonSchemaDependency.cs
+++ Manatee.Json/Schema/IJsonSchemaDependency.cs
@@ -1,29 +1,4 @@
-﻿/***************************************************************************************
-
-	Copyright 2016 Greg Dennis
-
-	   Licensed under the Apache License, Version 2.0 (the "License");
-	   you may not use this file except in compliance with the License.
-	   You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-	   Unless required by applicable law or agreed to in writing, software
-	   distributed under the License is distributed on an "AS IS" BASIS,
-	   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	   See the License for the specific language governing permissions and
-	   limitations under the License.
- 
-	File Name:		IJsonSchemaDependency.cs
-	Namespace:		Manatee.Json.Schema
-	Class Name:		IJsonSchemaDependency
-	Purpose:		Defines properties and methods required to represent dependencies
-					within JSON Schema.
-
-***************************************************************************************/
-using Manatee.Json.Serialization;
-
-namespace Manatee.Json.Schema
+﻿namespace Manatee.Json.Schema
 {
 	/// <summary>
 	/// Defines properties and methods required to represent dependencies within JSON Schema.
--- Manatee.Json/Serialization/PropertySelectionStrategy.cs
+++ Manatee.Json/Serialization/PropertySelectionStrategy.cs
@@ -1,28 +1,4 @@
-﻿/***************************************************************************************
-
-	Copyright 2016 Greg Dennis
-
-	   Licensed under the Apache License, Version 2.0 (the "License");
-	   you may not use this file except in compliance with the License.
-	   You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-	   Unless required by applicable law or agreed to in writing, software
-	   distributed under the License is distributed on an "AS IS" BASIS,
-	   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	   See the License for the specific language governing permissions and
-	   limitations under the License.
- 
-	File Name:		PropertySelectionStrategy.cs
-	Namespace:		Manatee.Json.Serialization
-	Class Name:		PropertySelectionStrategy
-	Purpose:		Enumerates the types of properties which are automatically
-					serialized.
-
-***************************************************************************************/
-
-using System;
+﻿using System;
 
 namespace Manatee.Json.Serialization
 {
