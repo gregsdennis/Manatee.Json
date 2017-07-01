03d101fc483c388601328c9965a837cc2a8c30ad
Greg Dennis <gregsdennis@yahoo.com>
2017-07-01 18:20:46 +1200

    updated bulid script and gitignore

--- build.bat
+++ build.bat
@@ -4,6 +4,7 @@ set config=%1
 if "%config%" == "" (
    set config=Release
 )
+set version=
 
 REM Restore packages
 call powershell "& .\nuget-restore.ps1"
@@ -22,11 +23,9 @@ if exist "%programfiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15
 REM *** NOTE *** When running this locally, use the version without the quotes.
 REM              MyGet requires the quotes, but the command line doesn't like them.  Don't ask me...
 
-REM call %msbuild% Manatee.Json.sln /p:Configuration="%config%" /m:1 /v:m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
-call "%msbuild%" Manatee.Json.sln /p:Configuration="%config%" /m:1 /v:m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+call %msbuild% Manatee.Json.sln /p:Configuration="%config%" /m:1 /v:m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 REM Package
-REM call %msbuild% Manatee.Json\Manatee.Json.csproj /t:pack /p:Configuration=Release
-call "%msbuild%" Manatee.Json\Manatee.Json.csproj /t:pack /p:Configuration=Release
+call %msbuild% Manatee.Json\Manatee.Json.csproj /t:pack /p:Configuration=Release
 
 copy Manatee.Json\bin\%config%\*.nupkg . /Y
