3f94fa9678fff6ff4c488880ec083598bb09e460
Greg Dennis <gregsdennis@yahoo.com>
2017-07-01 13:43:34 +1200

    edited build script

--- build.bat
+++ build.bat
@@ -1,12 +1,15 @@
 REM build script for MyGet builds
 
+REM *** NOTE *** When running this locally, make sure you have a ".localbuild" file in the root directory.
+REM              MyGet requires the quotes, but the command line doesn't like them.  Don't ask me...
+
 set config=%1
 if "%config%" == "" (
    set config=Release
 )
 
 REM Build
-dotnet restore
+dotnet restore "Manatee.Json\Manatee.Json.csproj"
 
 REM Detect MSBuild 15.0 path
 if exist "%programfiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" (
@@ -19,13 +22,13 @@ if exist "%programfiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15
     set msbuild="%programfiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
 )
 
-REM *** NOTE *** When running this locally, use the versionS without quotes around %msbuild%.
-REM              MyGet requires the quotes, but the command line doesn't like them.  Don't ask me...
-
-REM call "%msbuild%"" Manatee.Json.sln /p:Configuration="%config%" /m:1 /v:m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
-call %msbuild% Manatee.Json.sln /p:Configuration="%config%" /m:1 /v:m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
-
-REM Package
-
-REM call "%msbuild%" Manatee.Json.sln /t:pack /p:Configuration=Release
-call %msbuild% Manatee.Json.sln /t:pack /p:Configuration=Release
+if exist ".localbuild" (
+    call %msbuild% Manatee.Json.sln /p:Configuration="%config%" /m:1 /v:m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+    REM Package
+    call %msbuild% Manatee.Json.sln /t:pack /p:Configuration=Release
+)
+if not exist ".localbuild" (
+    call "%msbuild%" Manatee.Json.sln /p:Configuration="%config%" /m:1 /v:m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+    REM Package
+    call "%msbuild%" Manatee.Json.sln /t:pack /p:Configuration=Release
+)
