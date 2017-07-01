cb0e2cdce0444394dc32f94ebeee2aa31f799d4b
Greg Dennis <gregsdennis@yahoo.com>
2017-07-01 13:02:31 +1200

    updated nuget details.  updated build script

--- build.bat
+++ build.bat
@@ -22,10 +22,10 @@ if exist "%programfiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15
 REM *** NOTE *** When running this locally, use the versionS without quotes around %msbuild%.
 REM              MyGet requires the quotes, but the command line doesn't like them.  Don't ask me...
 
-REM call %msbuild% Manatee.Json.sln /p:Configuration="%config%" /m:1 /v:m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
-call "%msbuild%" Manatee.Json.sln /p:Configuration="%config%" /m:1 /v:m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+REM call "%msbuild%"" Manatee.Json.sln /p:Configuration="%config%" /m:1 /v:m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+call %msbuild% Manatee.Json.sln /p:Configuration="%config%" /m:1 /v:m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 REM Package
 
 REM call "%msbuild%" Manatee.Json.sln /t:pack /p:Configuration=Release
-call "%msbuild%" Manatee.Json.sln /t:pack /p:Configuration=Release
+call %msbuild% Manatee.Json.sln /t:pack /p:Configuration=Release
