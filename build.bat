59a8d9a2526e87e1b0d31bcb06975581dfc24119
Greg Dennis <gregsdennis@yahoo.com>
2017-07-01 18:46:40 +1200

    updated build process and file list.

--- build.bat
+++ build.bat
@@ -12,12 +12,15 @@ call powershell "& .\nuget-restore.ps1"
 REM Detect MSBuild 15.0 path
 if exist "%programfiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" (
     set msbuild="%programfiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+    set mstest="%programfiles(x86)%\Microsoft Visual Studio\2017\Community\Common7\IDE\MSTest.exe"
 )
 if exist "%programfiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" (
     set msbuild="%programfiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
+    set mstest="%programfiles(x86)%\Microsoft Visual Studio\2017\Professional\Common7\IDE\MSTest.exe"
 )
 if exist "%programfiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" (
     set msbuild="%programfiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
+    set mstest="%programfiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\MSTest.exe"
 )
 
 REM *** NOTE *** When running this locally, use the version without the quotes.
@@ -25,7 +28,9 @@ REM              MyGet requires the quotes, but the command line doesn't like th
 
 call %msbuild% Manatee.Json.sln /p:Configuration="%config%" /m:1 /v:m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
+REM Run Tests
+
+call %mstest% /testcontainer:Manatee.Json.Tests\bin\%config%\Manatee.Json.Tests.dll
+
 REM Package
 call %msbuild% Manatee.Json\Manatee.Json.csproj /t:pack /p:Configuration=Release
-
-copy Manatee.Json\bin\%config%\*.nupkg . /Y
