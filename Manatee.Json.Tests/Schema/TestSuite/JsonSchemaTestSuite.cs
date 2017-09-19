67262b0ab4ac6ec0cfd2e1f97548f183ab9752dc
Greg Dennis <gregsdennis@yahoo.com>
2017-09-19 14:16:37 +1200

    fixed schema test suite run issue. all tests run independently now. (#74)

--- Manatee.Json.Tests/Schema/TestSuite/JsonSchemaTestSuite.cs
+++ Manatee.Json.Tests/Schema/TestSuite/JsonSchemaTestSuite.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Runtime.Serialization.Json;
 using Manatee.Json.Schema;
 using Manatee.Json.Serialization;
 using NUnit.Framework;
@@ -19,33 +17,31 @@ namespace Manatee.Json.Tests.Schema.TestSuite
 		private const string RemotesFolder = @"..\..\..\Json-Schema-Test-Suite\remotes\";
 		private static readonly JsonSerializer _serializer;
 
-		private static IEnumerable<TestCaseData> _LoadSchema<T>(string testFolder)
-			where T : IJsonSchema
+		public static IEnumerable TestData04 => _LoadSchemaJson(Draft04TestFolder);
+		public static IEnumerable TestData06 => _LoadSchemaJson(Draft06TestFolder);
+
+		private static IEnumerable<TestCaseData> _LoadSchemaJson(string testFolder)
 		{
 			var testsPath = System.IO.Path.Combine(TestContext.CurrentContext.TestDirectory, testFolder).AdjustForOS();
 			var fileNames = Directory.GetFiles(testsPath, "*.json");
 
-			JsonSchemaFactory.SetDefaultSchemaVersion<T>();
-			var schemata = new List<TestCaseData>();
 			foreach (var fileName in fileNames)
 			{
 				var contents = File.ReadAllText(fileName);
 				var json = JsonValue.Parse(contents);
 
-				var testSets = _serializer.Deserialize<List<SchemaTestSet>>(json);
-
-				foreach (var testSet in testSets)
+				foreach (var testSet in json.Array)
 				{
-					foreach (var test in testSet.Tests)
+					var schemaJson = testSet.Object["schema"];
+					foreach (var testJson in testSet.Object["tests"].Array)
 					{
-						var testName = $"{testSet.Description}.{test.Description}".Replace(' ', '_');
-						schemata.Add(new TestCaseData(testSet.Schema, test, fileName, testName, typeof(T)) {TestName = testName});
+						var testName = $"{testSet.Object["description"]}.{testJson.Object["description"]}".Replace(' ', '_');
+						yield return new TestCaseData(fileName, testJson, schemaJson, testName) {TestName = testName};
 					}
 				}
 			}
-			return schemata;
 		}
-
+		
 		static JsonSchemaTestSuite()
 		{
 			_serializer = new JsonSerializer();
@@ -76,57 +72,32 @@ namespace Manatee.Json.Tests.Schema.TestSuite
 			JsonSchemaOptions.Download = null;
 		}
 
-		// This should really be run using a TestCaseSource that supplies all of the tests
-		// as cases so that NUnit can run each one independently.  However there's an issue
-		// in the registry in that deserialization auto-registers any schema with an ID and
-		// when the $ref tests for draft-06 deserialize, they overwrite the registrations
-		// for the draft-04 tests.  This causes two of the draft-04 tests to fail.  To get
-		// around this for now, I've reverted back to running them in one go so that I can
-		// properly separate the drafts.
-		[Test]
-		public void Run04()
-		{
-			JsonSchemaRegistry.Clear();
-			var testData = _LoadSchema<JsonSchema04>(Draft04TestFolder);
-
-			foreach (var test in testData)
-			{
-				_Run((IJsonSchema) test.Arguments[0],
-				     (SchemaTest) test.Arguments[1],
-				     (string) test.Arguments[2],
-				     (string) test.Arguments[3],
-				     (Type) test.Arguments[4]);
-			}
-		}
-
-		// This should really be run using a TestCaseSource that supplies all of the tests
-		// as cases so that NUnit can run each one independently.  However there's an issue
-		// in the registry in that deserialization auto-registers any schema with an ID and
-		// when the $ref tests for draft-06 deserialize, they overwrite the registrations
-		// for the draft-04 tests.  This causes two of the draft-04 tests to fail.  To get
-		// around this for now, I've reverted back to running them in one go so that I can
-		// properly separate the drafts.
-		[Test]
-		public void Run06()
-		{
-			JsonSchemaRegistry.Clear();
-			var testData = _LoadSchema<JsonSchema06>(Draft06TestFolder);
-
-			foreach (var test in testData)
-			{
-				_Run((IJsonSchema) test.Arguments[0],
-				     (SchemaTest) test.Arguments[1],
-				     (string) test.Arguments[2],
-				     (string) test.Arguments[3],
-				     (Type) test.Arguments[4]);
-			}
-		}
-
-		private static void _Run(IJsonSchema schema, SchemaTest test, string fileName, string testName, Type schemaType)
+		[TestCaseSource(nameof(TestData04))]
+		public void Run04(string fileName, JsonValue testJson, JsonValue schemaJson, string testname)
 		{
 			Console.WriteLine(fileName);
 
-			JsonSchemaFactory.SetDefaultSchemaVersion(schemaType);
+			JsonSchemaFactory.SetDefaultSchemaVersion<JsonSchema04>();
+			_Run<JsonSchema04>(fileName, testJson, schemaJson);
+		}
+
+		[TestCaseSource(nameof(TestData06))]
+		public void Run06(string fileName, JsonValue testJson, JsonValue schemaJson, string testname)
+		{
+			Console.WriteLine(fileName);
+
+			JsonSchemaFactory.SetDefaultSchemaVersion<JsonSchema06>();
+			_Run<JsonSchema06>(fileName, testJson, schemaJson);
+		}
+
+		private static void _Run<T>(string fileName, JsonValue testJson, JsonValue schemaJson)
+			where T : IJsonSchema
+		{
+			Console.WriteLine(fileName);
+
+			JsonSchemaFactory.SetDefaultSchemaVersion<T>();
+			var test = _serializer.Deserialize<SchemaTest>(testJson);
+			var schema = _serializer.Deserialize<IJsonSchema>(schemaJson);
 			var results = schema.Validate(test.Data);
 
 			if (test.Valid != results.Valid)
@@ -139,16 +110,15 @@ namespace Manatee.Json.Tests.Schema.TestSuite
 		public void RunOne()
 		{
 			var testName = "propertyNames_validation.some_property_names_invalid";
-			var schemaVersion = typeof(JsonSchema06);
 
-			var selectedTest = _LoadSchema<JsonSchema06>(Draft06TestFolder)
+			var selectedTest = _LoadSchemaJson(Draft06TestFolder)
 				.First(d => Equals(d.Arguments[3], testName));
 
-			_Run((IJsonSchema) selectedTest.Arguments[0],
-			     (SchemaTest) selectedTest.Arguments[1],
-			     (string) selectedTest.Arguments[2],
-			     (string) selectedTest.Arguments[3],
-			     schemaVersion);
+			// ReSharper disable PossibleInvalidCastException
+			_Run<JsonSchema06>((string) selectedTest.Arguments[0],
+			                   (JsonValue) selectedTest.Arguments[0],
+			                   (JsonValue) selectedTest.Arguments[0]);
+			// ReSharper restore PossibleInvalidCastException
 		}
 	}
 }
