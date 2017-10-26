4bf28b735f7942bbb7dbb43def7a50ce7485fbde
gregsdennis <gregsdennis@yahoo.com>
2017-10-26 19:29:03 +1300

    updated build script

--- build.bat
+++ build.bat
@@ -1,12 +1,15 @@
 REM build script for MyGet builds
 
+REM *** NOTE *** When running this locally, remove the quotes from around the executable variables (e.g. %GitPath%).
+REM              MyGet requires the quotes, but the command line doesn't like them.  Don't ask me...
+
 REM Get latest for test suites
 cd json-patch-tests
-%GitPath% pull
+"%GitPath%" pull
 cd ../Json-Path-Test-Suite
-%GitPath% pull
+"%GitPath%" pull
 cd ../Json-Schema-Test-Suite
-%GitPath% pull
+"%GitPath%" pull
 cd ..
 
 REM Start build
@@ -30,19 +33,16 @@ if exist "%programfiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15
     set msbuild="%programfiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
 )
 
-REM *** NOTE *** When running this locally, use the version without the quotes.
-REM              MyGet requires the quotes, but the command line doesn't like them.  Don't ask me...
-
+REM Run build
 call "%msbuild%" Manatee.Json.sln /p:Configuration="%config%" /m:1 /v:m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 if not "%errorlevel%"=="0" goto failure
 
-REM Run Tests
-
+REM Run tests
 set nunit="packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console.exe"
 "%nunit%" Manatee.Json.Tests\bin\%config%\Manatee.Json.Tests.dll
 if not "%errorlevel%"=="0" goto failure
 
-REM Package
+REM package
 call "%msbuild%" Manatee.Json\Manatee.Json.csproj /t:pack /p:Configuration=Release
 if not "%errorlevel%"=="0" goto failure
 
