1e2cfe763ceab84ab6eef3db5f47af7c985c0c38
gregsdennis <gregsdennis@yahoo.com>
2017-10-31 11:43:40 +1300

    added schema serialization attribute

--- Manatee.Json.Tests/DevTest.cs
+++ Manatee.Json.Tests/DevTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Manatee.Json.Internal;
 using Manatee.Json.Patch;
 using Manatee.Json.Schema;
+using Manatee.Json.Serialization;
 using Manatee.Json.Tests.Schema.TestSuite;
 using NUnit.Framework;
 
---
+++ Manatee.Json/Serialization/SchemaAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Manatee.Json.Serialization
+{
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+	public class SchemaAttribute : Attribute
+	{
+		public string Source { get; }
+
+		public SchemaAttribute(string source)
+		{
+			Source = source;
+		}
+	}
+}
