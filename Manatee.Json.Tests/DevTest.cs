c16174579c95223b29c03c2eae62480318674cc1
gregsdennis <gregsdennis@yahoo.com>
2017-11-02 21:49:01 +1300

    created dev test

--- Manatee.Json.Tests/DevTest.cs
+++ Manatee.Json.Tests/DevTest.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using Manatee.Json.Internal;
 using Manatee.Json.Patch;
 using Manatee.Json.Schema;
+using Manatee.Json.Serialization;
 using Manatee.Json.Tests.Schema.TestSuite;
 using NUnit.Framework;
 
 namespace Manatee.Json.Tests
 {
 	[TestFixture]
-	[Ignore("This test fixture for development purposes only.")]
+	//[Ignore("This test fixture for development purposes only.")]
 	public class DevTest
 	{
 		[Test]
 		// TODO: Add categories to exclude this test.
 		public void Test1()
 		{
-			Console.WriteLine(JsonPatch.Schema.ToJson(null).GetIndentedString());
+			var list = new List<object> {1, false, "hello"};
+
+			var serializer = new JsonSerializer();
+			var json = serializer.Serialize(list);
+
+			Console.WriteLine(json);
 		}
 	}
 }
