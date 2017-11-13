6be6543203c267d2516ea4f05be5dcfb8d38a208
gregsdennis <gregsdennis@yahoo.com>
2017-11-14 10:11:35 +1300

    updates

--- Manatee.Json/JsonOptions.cs
+++ Manatee.Json/JsonOptions.cs
@@ -4,7 +4,6 @@
 	{
 		public static char PrettyPrintIndentChar { get; set; } = '\t';
 		public static DuplicateKeyBehavior DuplicateKeyBehavior { get; set; }
-		public static NullEqualityBehavior NullEqualityBehavior { get; set; }
 		internal static bool AllowTrailingCommas { get; set; }
 		public static ArrayEquality DefaultArrayEquality { get; set; }
 		public static bool ThrowOnIncorrectTypeAccess { get; set; } = true;
