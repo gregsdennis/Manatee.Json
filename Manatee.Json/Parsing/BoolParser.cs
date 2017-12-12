58e663b01467aba2456fb045f03b6d4ab2829c3a
Christopher Watford <christopher.watford@ge.com>
2017-12-12 12:27:28 -0500

    Fix Pex issues with BoolParser

--- Manatee.Json/Parsing/BoolParser.cs
+++ Manatee.Json/Parsing/BoolParser.cs
@@ -17,6 +17,11 @@ namespace Manatee.Json.Parsing
 
 		public string TryParse(string source, ref int index, out JsonValue value, bool allowExtraChars)
 		{
+			if (source == null)
+				throw new ArgumentNullException(nameof(source));
+			if (index >= source.Length)
+				throw new ArgumentOutOfRangeException(nameof(index));
+
 			value = null;
 
 			if (source[index] == 't' || source[index] == 'T')
@@ -47,6 +52,9 @@ namespace Manatee.Json.Parsing
 
 		public string TryParse(TextReader stream, out JsonValue value)
 		{
+			if (stream == null)
+				throw new ArgumentNullException(nameof(stream));
+
 			value = null;
 
 			int count;
@@ -93,6 +101,9 @@ namespace Manatee.Json.Parsing
 
 		public async Task<(string errorMessage, JsonValue value)> TryParseAsync(TextReader stream, CancellationToken token)
 		{
+			if (stream == null)
+				throw new ArgumentNullException(nameof(stream));
+
 			var buffer = SmallBufferCache.Acquire(5);
 			var count = await stream.ReadAsync(buffer, 0, 4);
 			if (count < 4)
