9aaadb1b3a051d6992be77c631ba241b23248d0e
gregsdennis <gregsdennis@yahoo.com>
2018-01-10 01:02:13 +1300

    initial implementation; no escape sequences supported

---
+++ Manatee.Json/Pointer/JsonPointer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Manatee.Json.Pointer
+{
+	public class JsonPointer : List<string>
+	{
+		public JsonPointer() { }
+		public JsonPointer(IEnumerable<string> source) : base(source) { }
+
+		public static JsonPointer Parse(string source)
+		{
+			return new JsonPointer(source.Split('/').Skip(1));
+		}
+
+		public JsonValue Evaluate(JsonValue root)
+		{
+			var current = root;
+			foreach (var segment in this)
+			{
+				current = _EvaulateSegment(current, segment);
+				if (current == null) break;
+			}
+
+			return current;
+		}
+
+		private JsonValue _EvaulateSegment(JsonValue current, string segment)
+		{
+			if (current.Type == JsonValueType.Array && int.TryParse(segment, out var index))
+				return index < current.Array.Count
+					       ? current.Array[index]
+					       : null;
+
+			return current.Type != JsonValueType.Object || !current.Object.TryGetValue(segment, out var value)
+				       ? null
+				       : value;
+		}
+	}
+}
--- Manatee.Json/Schema/JsonSchemaReference.cs
+++ Manatee.Json/Schema/JsonSchemaReference.cs
@@ -212,12 +212,11 @@ namespace Manatee.Json.Schema
 				if (value.Type == JsonValueType.Object)
 				{
 					if (!value.Object.ContainsKey(unescaped)) return null;
-					JsonValue id;
 					// There's not really another way to do this well without the reference knowing what
 					// version schema it should be using at each step in the path, so we test for both.
 					// Since draft-06's '$id' is less likely to be used as a regular JSON property, we
 					// check it first.
-					if (value.Object.TryGetValue("$id", out id) || value.Object.TryGetValue("id", out id))
+					if (value.Object.TryGetValue("$id", out var id) || value.Object.TryGetValue("id", out id))
 					{
 						documentPath = Uri.TryCreate(id.String, UriKind.Absolute, out Uri uri)
 							               ? uri
