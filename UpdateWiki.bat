717a766768454e5c9a4ec54c7aac204adcb552b9
gregsdennis <gregsdennis@yahoo.com>
2018-03-16 18:43:49 +1300

    updated wiki gen script

--- UpdateWiki.bat
+++ UpdateWiki.bat
@@ -8,16 +8,11 @@ git pull
 
 dotnet run -p AutoWiki.csproj ..\..\Wiki
 
-goto done
-
 cd ..\..\Wiki
 git add -A
 git commit -m "updated after build"
 git push
 
 cd ..
-
-:done
-cd ..\..
 
 rd /s /q AutoWiki
