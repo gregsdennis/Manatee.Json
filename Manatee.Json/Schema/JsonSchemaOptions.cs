16f985edaf7c8330e8c3882f5aca174bd6c06e22
Greg Dennis <gregsdennis@yahoo.com>
2018-12-04 08:15:08 +1300

    added examples to ref resolution options

--- Manatee.Json/Schema/JsonSchemaOptions.cs
+++ Manatee.Json/Schema/JsonSchemaOptions.cs
@@ -48,12 +48,12 @@ namespace Manatee.Json.Schema
 		/// </summary>
 		/// <remarks>
 		/// As of draft-08, keywords are allowed to be adjacent to <code>$ref</code>.  This means that an
-		/// adjacent <code>$id</code> keyword will now change the URI base whereas in prior drafts it would not
+		/// adjacent <code>$id</code> keyword will now change the base URI whereas in prior drafts it would not
 		/// since adjacent keywords were to be ignored.
 		///
-		/// When Manatee.Json cannot determine the draft a particular schema is using (usually specified via
-		/// the <code>$schema</code> keyword), this option will determine the behavior for resolving URIs in these
-		/// cases.
+		/// When Manatee.Json cannot determine the draft a particular schema is using (determined via
+		/// the <code>$schema</code> keyword or the selection of keywords being used), this option will
+		/// determine the behavior for resolving URIs.
 		/// </remarks>
 		public static RefResolutionStrategy RefResolution { get; set; } = RefResolutionStrategy.IgnoreSiblingId;
 
@@ -93,11 +93,45 @@ namespace Manatee.Json.Schema
 		/// Sibling <code>$id</code> properties will be ignored and will not change the
 		/// base URI.  This will process <code>$ref</code> according to draft-07 and earlier.
 		/// </summary>
+		/// <example>
+		/// The <code>$ref</code> in the following schema will resolve to <i>http://example.com/document.json</i>
+		/// because the <code>$id</code> of <i>folder</i> will be ignored.
+		/// 
+		/// <code>
+		/// {
+		///   "$schema": "http://json-schema.org/draft-08/schema#",
+		///   "$id": "http://example.com/root.json"
+		///   "properties": {
+		///     "prop": {
+		///       "$id": "folder"
+		///       "$ref": "document.json"
+		///     }
+		///   }
+		/// }
+		/// </code>
+		/// </example>
 		IgnoreSiblingId,
 		/// <summary>
 		/// Sibling <code>$id</code> properties will be processed and will change the
 		/// base URI.  This will process <code>$ref</code> according to draft-08.
 		/// </summary>
+		/// <example>
+		/// The <code>$ref</code> in the following schema will resolve to <i>http://example.com/folder/document.json</i>
+		/// because the <code>$id</code> of <i>folder</i> will be processed.
+		/// 
+		/// <code>
+		/// {
+		///   "$schema": "http://json-schema.org/draft-08/schema#",
+		///   "$id": "http://example.com/root.json"
+		///   "properties": {
+		///     "prop": {
+		///       "$id": "folder"
+		///       "$ref": "document.json"
+		///     }
+		///   }
+		/// }
+		/// </code>
+		/// </example>
 		ProcessSiblingId
 	}
 }
