d1a576d1a7fe710ab1778e516dfda21c1d42dc04
Greg Dennis <gregsdennis@yahoo.com>
2019-09-18 17:06:57 +1200

    import new meta-schemas; assert incl if offline

--- Manatee.Json.Tests/JsonOrgTests.cs
+++ Manatee.Json.Tests/JsonOrgTests.cs
@@ -10,13 +10,22 @@ namespace Manatee.Json.Tests
 		[Test]
 		public void JsonCheckerTest()
 		{
-			var text = new WebClient().DownloadString("http://www.json.org/JSON_checker/test/pass1.json");
-			Console.WriteLine($"{text}\n");
+			string text;
+			try
+			{
+				text = new WebClient().DownloadString("http://www.json.org/JSON_checker/test/pass1.json");
+				Console.WriteLine($"{text}\n");
 
-			// Just make sure the parser doesn't fail.
-			var json = JsonValue.Parse(text);
+				// Just make sure the parser doesn't fail.
+				var json = JsonValue.Parse(text);
 
-			Console.WriteLine(json);
+				Console.WriteLine(json);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+				Assert.Inconclusive("Cannot contact server");
+			}
 		}
 	}
 }
